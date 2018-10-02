7380a5360b8e5b25f5bd162e7f769d28d68c156b
Elchyn Cheliabiyeu 650504 <chelyabiev.elchin@gmail.com>
2018-10-02 23:12:59 +0300

    PostgreSQL integration

--- markup/settings.py
+++ markup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'markup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'markup_db',
+        'USER': 'elchyn',
+        'PASSWORD': 'kewbr',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
