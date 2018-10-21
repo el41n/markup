241b4c3e66a5aaeb08944ff911350bfed2e4287b
Elchyn Cheliabiyeu 650504 <chelyabiev.elchin@gmail.com>
2018-10-21 15:20:24 +0300

    Added Text editing

--- src/front-end/markup/src/app/app.module.ts
+++ src/front-end/markup/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageComponent } from './message/message.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent,
+    DashboardComponent,
+    TextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
