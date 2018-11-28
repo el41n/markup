ad257fb42d926f8ec8924b486d925d35f33b4a32
Elchyn Cheliabiyeu 650504 <chelyabiev.elchin@gmail.com>
2018-11-28 23:18:11 +0300

    Front end refactored register component

--- src/front-end/markup/src/app/register/register.component.ts
+++ src/front-end/markup/src/app/register/register.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-
-import { AlertService } from "../services/alert.service";
-import { UserService } from "../services/user.service";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {first} from 'rxjs/operators';
+import {AlertService} from "../services/alert.service";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-register',
@@ -12,48 +11,49 @@ import { UserService } from "../services/user.service";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-    registerForm: FormGroup;
-    loading = false;
-    submitted = false;
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private router: Router,
-        private userService: UserService,
-        private alertService: AlertService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private alertService: AlertService) {
+  }
 
-    ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            email: ['', Validators.required, Validators.email],
-            password1: ['', [Validators.required, Validators.minLength(6)]],
-            password2: ['', [Validators.required, Validators.minLength(6)]],
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password1: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
         });
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.userService.register(this.registerForm.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+  }
 }
 
